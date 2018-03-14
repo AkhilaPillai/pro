a82f54be3ecf8774fdc69dd28248291f5e828e95
AkhilaPillai <36732292+AkhilaPillai@users.noreply.github.com>
2018-03-14 17:44:34 +0530

    Create str.cpp

---
+++ str.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char str[100];
+    cout << "Enter a string: ";
+    gets(str);
+    cout << "You entered: " << str;
+    
+    return 0;
+}
